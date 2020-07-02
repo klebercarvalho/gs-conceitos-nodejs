7c9b9c6781b4a92a4752e34141097292cb60a4ab
Kleber Rodrigo de Carvalho <kleber.carvalho@ibm.com>
2020-07-01 23:38:03 -0300

    Node API REST

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3333;
+
+app.get('/project', (request, response) => {
+  return response.json({ message: 'Hello World!' });
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
